bf0680c6c764f3ef365d6b0ace1cc069d55b5021
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-20 04:59:32 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import Home from "./pages/Home";
+import {Routes, Route} from 'react-router-dom'
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">Tailwind is working!</h1>
-      <h1 className="text-2xl text-red-700">This is a demo Pgae for react js</h1>
-      <p className="text-2xl italic text-green-800">Here installed: React js, Tailwind css, react icon, reatc dom, framer motion, formik</p>
+      <Home/>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+
     </div>
   );
 }
