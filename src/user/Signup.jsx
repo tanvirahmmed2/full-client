b68a418c05197e417d60d7a873bab93fcdd9e467
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-26 01:11:54 +0600

    update

--- src/user/Signup.jsx
+++ src/user/Signup.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom"
 
 const Signup = () => {
   return (
-    <div className='w-full h-[80vh]'>
-      <div className='w-3/4 h-[600px] m-auto px-10 gap-6 py-14 flex flex-col items-center justify-center '>
-        <h1>SignUp</h1>
+    <div className='w-full h-[80vh] flex items-center justify-center'>
+      <div className='w-[400px] p-4  h-[600px] m-auto px-10 gap-6 py-14 flex flex-col items-start justify-center '>
+        <h1 className='text-3xl font-semibold text-red-600'>SignUp</h1>
         <div className='w-full flex flex-col items-center gap-4 justify-center'>
-          <input className='border-2 px-2 p-1 rounded-xl w-1/2 outline-none' type="text" placeholder='name' />
-          <input className='border-2 px-2 p-1 rounded-xl w-1/2 outline-none' type="email" placeholder='email address' />
-          <input className='border-2 px-2 p-1 rounded-xl w-1/2 outline-none' type="password" placeholder='password' />
+          <input className='border-2 px-4 p-2 rounded-xl w-full outline-none' type="text" placeholder='name' />
+          <input className='border-2 px-4 p-2 rounded-xl w-full outline-none' type="email" placeholder='email address' />
+          <input className='border-2 px-4 p-2 rounded-xl w-full outline-none' type="password" placeholder='password' />
         </div>
-        <button className='p-1 px-2 border-2 rounded-xl'>Continue</button>
+        <button className='p-1 px-2  rounded-xl w-full bg-red-600 text-white'>Continue</button>
         <Link to="/login">already user? Login Here</Link>
         <div className='w-full flex flex-row items-center justify-center gap-4'>
           <input type="checkbox" name='' />
