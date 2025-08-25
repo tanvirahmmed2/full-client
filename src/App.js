a59926d22d9547cf80072d53c4dfa24763458361
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-25 16:47:35 +0600

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Login from "./user/Login";
 import Footer from "./layouts/Footer";
 import Navbar from "./layouts/Navbar";
 import Shop from './layouts/Shop';
+import ShopCategory from './pages/ShopCategory';
+import Products from './pages/Products';
+import Cart from './pages/Cart';
+import Signup from './user/Signup';
 
 function App() {
   return (
@@ -12,6 +16,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Shop/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/mens" element={<ShopCategory category="mens"/>}/>
+        <Route path="/womens" element={<ShopCategory category="womens"/>}/>
+        <Route path="/kids" element={<ShopCategory category="kids"/>}/>
+        <Route path='/product' element={<Products/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/signup' element={<Signup/>}/>
       </Routes>
       <Footer/>
 
--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -7,15 +7,15 @@ const Navbar = () => {
     <div className='fixed shadow-lg top-0 w-full h-14 px-6 flex flex-row items-center justify-between'>
       <a href="/" className=' font-bold text-3xl'>Client</a>
       <div className='w-auto h-14 hidden lg:flex items-center justify-center gap-2'>
-        <Link className=' px-6 h-14 flex items-center hover:border-b-2'>Shop</Link>
-        <Link className=' px-6 h-14 flex items-center hover:border-b-2'>Men</Link>
-        <Link className=' px-6 h-14 flex items-center hover:border-b-2'>Women</Link>
-        <Link className=' px-6 h-14 flex items-center hover:border-b-2'>Kids</Link>
+        <Link to="/" className=' px-6 h-14 flex items-center hover:border-b-2'>Shop</Link>
+        <Link to="/mens" className=' px-6 h-14 flex items-center hover:border-b-2'>Men</Link>
+        <Link to="/womens" className=' px-6 h-14 flex items-center hover:border-b-2'>Women</Link>
+        <Link to="/kids" className=' px-6 h-14 flex items-center hover:border-b-2'>Kids</Link>
 
       </div>
       <div className='w-auto h-14 flex flex-row items-center justify-center gap-4'>
-        <Link to="login"className='px-4 h-8 border-2 rounded-2xl  flex items-center'>Login</Link>
-        <Link to="cart" className='px-4 h-14 flex items-center text-3xl'><CiShoppingCart/></Link>
+        <Link to="/login"className='px-4 h-8 border-2 rounded-2xl  flex items-center'>Login</Link>
+        <Link to="/cart" className='px-4 h-14 flex items-center text-3xl'><CiShoppingCart/></Link>
       </div>
     </div>
   )
