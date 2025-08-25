a1313426b4f02941edb1337f3bb926d59df38be1
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-26 00:47:42 +0600

    update

--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -4,23 +4,27 @@ import dropdown_icon from "../components/Assets/dropdown_icon.png"
 import Item from "../components/Item"
 
 const ShopCategory = (props) => {
-  const {all_products}= useContext(ShopContext)
+  const { all_products } = useContext(ShopContext)
   return (
-    <div>
-      <img src={props.banner} alt="" />
-      <div>
-        <p>
-          <span>Showing 1-12</span>
-        </p>
-        <div>
-          Sort by <img src={dropdown_icon} alt="" />
+    <div className='w-full flex flex-col items-center justify-center gap-6'>
+      <img src={props.banner} alt="" className='' />
+      <div className='w-full flex flex-col gap-8 items-center justify-between'>
+
+        <div className='flex flex-row items-center justify-between gap-8'>
+          <p>
+            <span>Showing 1-12</span>
+          </p>
+          <p className='flex flex-row items-center justify-center gap-4'>
+            Sort by <img src={dropdown_icon} alt="" />
+
+          </p>
         </div>
         <div className='w-full grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-6 '>
-          {all_products.map((item)=>{
-            if(props.category === item.category){
-              return <Item  key= {item.id} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+          {all_products.map((item) => {
+            if (props.category === item.category) {
+              return <Item key={item.id} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
             }
-            else{
+            else {
               return null
             }
           })}
