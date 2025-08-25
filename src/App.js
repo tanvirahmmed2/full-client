a24adba90867c79c0c93ff4320dd84c1b40ba455
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-25 16:38:55 +0600

    update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import {Routes, Route} from 'react-router-dom'
 import Login from "./user/Login";
 import Footer from "./layouts/Footer";
 import Navbar from "./layouts/Navbar";
-import Home from './layouts/Home';
+import Shop from './layouts/Shop';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center relative">
       <Navbar/>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Shop/>}/>
         <Route path="/login" element={<Login/>}/>
       </Routes>
       <Footer/>
--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import { CiShoppingCart } from "react-icons/ci";
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='fixed shadow-lg top-0 w-full h-14 px-6 flex flex-row items-center justify-between'>
+      <a href="/" className=' font-bold text-3xl'>Client</a>
+      <div className='w-auto h-14 hidden lg:flex items-center justify-center gap-2'>
+        <Link className=' px-6 h-14 flex items-center hover:border-b-2'>Shop</Link>
+        <Link className=' px-6 h-14 flex items-center hover:border-b-2'>Men</Link>
+        <Link className=' px-6 h-14 flex items-center hover:border-b-2'>Women</Link>
+        <Link className=' px-6 h-14 flex items-center hover:border-b-2'>Kids</Link>
+
+      </div>
+      <div className='w-auto h-14 flex flex-row items-center justify-center gap-4'>
+        <Link to="login"className='px-4 h-8 border-2 rounded-2xl  flex items-center'>Login</Link>
+        <Link to="cart" className='px-4 h-14 flex items-center text-3xl'><CiShoppingCart/></Link>
+      </div>
     </div>
   )
 }
