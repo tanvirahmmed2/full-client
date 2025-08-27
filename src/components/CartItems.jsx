42fbfade83365f0c13530538d56e802bcc8aed1d
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-27 10:39:47 +0600

    update

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -21,16 +21,16 @@ const CartItems = () => {
         if (cartItems[e.id] > 0) {
           return (
             <div key={e.id} className="w-full">
-              <div className="w-full grid grid-cols-6 h-20 overflow-hidden border-b-2 border-black">
-                <div className="w-full flex items-center justify-center">
+              <div className="w-full grid grid-cols-6 justify-items-center overflow-hidden border-b-2 border-black">
+                <div className=" flex items-center justify-center">
                   <img src={e.image} alt="producticon" className="w-auto h-14 text-center" />
 
                 </div>
-                <p className="w-full flex items-center justify-center ">{e.name}</p>
-                <p className="w-full flex items-center justify-center ">{e.new_price}</p>
-                <button className="w-full flex items-center justify-center  ">{cartItems[e.id]}</button>
-                <p className=" w-full flex items-center justify-center">{e.new_price * cartItems[e.id]}</p>
-                <div className="w-full flex items-center justify-center">
+                <p className=" flex items-center justify-center ">{e.name}</p>
+                <p className=" flex items-center justify-center ">{e.new_price}</p>
+                <button className=" px-4 my-5 rounded-lg flex items-center justify-center  border-2">{cartItems[e.id]}</button>
+                <p className="  flex items-center justify-center">{e.new_price * cartItems[e.id]}</p>
+                <div className=" flex items-center justify-center">
                   <img src={remove_icon} alt="producticon" className="w-auto  text-center" onClick={()=>{removeFromCart(e.id)}} />
 
                 </div>
