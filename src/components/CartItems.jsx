e8044299203e19313bd8c05c69a766b5da945ded
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-08-27 10:55:41 +0600

    update

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -27,9 +27,9 @@ const CartItems = () => {
 
                 </div>
                 <p className=" flex items-center justify-center ">{e.name}</p>
-                <p className=" flex items-center justify-center ">{e.new_price}</p>
+                <p className=" flex items-center justify-center ">${e.new_price}</p>
                 <button className=" px-4 my-5 rounded-lg flex items-center justify-center  border-2">{cartItems[e.id]}</button>
-                <p className="  flex items-center justify-center">{e.new_price * cartItems[e.id]}</p>
+                <p className="  flex items-center justify-center">${e.new_price * cartItems[e.id]}</p>
                 <div className=" flex items-center justify-center">
                   <img src={remove_icon} alt="producticon" className="w-auto  text-center" onClick={()=>{removeFromCart(e.id)}} />
 
@@ -40,6 +40,34 @@ const CartItems = () => {
         }
         return null;
       })}
+      <div className="flex gap-20 w-full flex-row items-center justify-center">
+        <div className="flex-1 flex flex-col  gap-5 w-full">
+          
+          <h1 className="text-2xl font-semibold ">Cart total</h1>
+          <div className="flex w-full flex-col">
+            <div className="flex flex-row items-center justify-between border-b-2 p-2">
+              <p>Subtotal</p>
+              <p>${0}</p>
+            </div>
+            <div className="flex flex-row items-center justify-between border-b-2 p-2">
+              <p>Shipping Fee</p>
+              <p>Free</p>
+            </div>
+            <div className="flex flex-row items-center justify-between border-b-2 p-2">
+              <h3>Total</h3>
+              <h3>${0}</h3>
+            </div>
+          </div>
+          <button className="bg-red-500 text-white rounded-xl p-1 hover:scale-[1.01]">Proceed to checkout</button>
+        </div>
+        <div className="w-full flex-1 flex flex-col gap-4 p-2 items-start border-2 rounded-lg  justify-center">
+          <p>If you have a promo code enter here</p>
+          <div className="w-full flex-row flex gap-2">
+            <input type="text" placeholder="promo code" className="p-1 w-full px-2 rounded-lg outline-none border-2"/>
+            <button className="bg-red-500 text-white rounded-xl p-1 px-4 hover:scale-[1.01]">Submit</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
