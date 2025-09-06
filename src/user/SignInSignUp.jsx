34b4703e277e429d1f1ae2e0523e449016896ea6
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-09-06 07:53:30 +0600

    update

--- src/user/SignInSignUp.jsx
+++ src/user/SignInSignUp.jsx
@@ -5,12 +5,20 @@ const SignInSignUp = () => {
     
     
     const [state, setState] = useState('SignIn')
-
+    const [formData, setFormData]= useState({
+        username: '',
+        email: '',
+        password: ''
+    })
+    
+    const changehandler=(e)=>{
+        setFormData({...formData,[e.target.name]: e.target.value})
+    }
     const signin= async()=>{
-        
+        console.log('signin function executed', formData)
     }
     const signup= async()=>{
-
+        console.log('signup function executed', formData)
     }
 
 
@@ -19,9 +27,9 @@ const SignInSignUp = () => {
             <div className='w-[400px]  h-[600px] m-auto p-10 gap-6  flex flex-col items-center justify-center bg-white border-red-500 border-2 rounded-xl'>
                 <h1 className='text-3xl font-semibold text-red-600'>{state}</h1>
                 <div className='w-full flex flex-col items-center gap-4 justify-center'>
-                    {state === "SignUp" ? <input className='border-2 px-4 p-2 rounded-xl w-full outline-none' type="text" placeholder='name' /> : <></>}
-                    <input className='border-2 px-4 p-2 rounded-xl w-full outline-none' type="email" placeholder='email address' />
-                    <input className='border-2 px-4 p-2 rounded-xl w-full outline-none' type="password" placeholder='password' />
+                    {state === "SignUp" ? <input className='border-2 px-4 p-2 rounded-xl w-full outline-none' type="text" placeholder='username' name='username' value={formData.username} onChange={changehandler}  /> : <></>}
+                    <input className='border-2 px-4 p-2 rounded-xl w-full outline-none' type="email" name='email' placeholder='email address' value={formData.email} onChange={changehandler} />
+                    <input className='border-2 px-4 p-2 rounded-xl w-full outline-none' type="password" name='password' placeholder='password' value={formData.password} onChange={changehandler} />
                 </div>
                 <button onClick={()=>{state==='SignIn'? signin(): signup()}} className='p-1 px-2  rounded-xl w-full bg-red-600 text-white'>Continue</button>
                 {state === "SignUp" ? <p onClick={() => { setState('SignIn') }} className='cursor-pointer'>already user? Login Here</p> : <></>}
